46e96c1e116f97b2f0ba7272401a3fc13664515a
skzxx <rrachmawan1965@gmail.com>
2021-09-11 09:44:48 +0700

    pussy

---
+++ Program_Python.py
@@ -0,0 +1,20 @@
+#01 - printing the var value
+print("01 - printing the value")
+a = 10
+print (a, "\n")
+print("02 - multi print")
+b = 12
+print("this is the b value: ", b, "\n")
+print("03 - Naming the variable")
+Operator_B_Value = 77
+print("This is the value of B-Operator", Operator_B_Value,"\n")
+print("04 - Assignment Indirect Operator")
+Operator_Indirect_A = 6
+Operator_Indirect_B = Operator_Indirect_A
+print("Value of the operator indirect B equal to ", Operator_Indirect_B)
+
+
+
+
+
+
